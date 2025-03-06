a322d06fc98b201a622c47da634c1ca73e96f67c
onionbulbperception <ezpr.flip303@passinbox.com>
2025-03-06 06:55:41 +0200

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '',
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
     //console.log(event.target.value)
@@ -19,6 +20,11 @@ const App = () => {
   const handleNumberChange = (event) => {
     //console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChanges = (event) => {
+    //console.log(event.target.value)
+    setFilter(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -43,9 +49,20 @@ const App = () => {
     setNewNumber('')
   }
 
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLocaleLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input value={filter} onChange={handleFilterChanges}></input>
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange}/> <br/>
@@ -56,7 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
