99ef78e862191d3fe37993ba483e1b69e6047907
onionbulbperception <ezpr.flip303@passinbox.com>
2025-03-16 12:47:21 +0200

    4.4: Helper Functions and Unit Tests, step 2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,9 @@
 const dummy = (blogs) => {
     return 1
 }
-  
-module.exports = { dummy }
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
