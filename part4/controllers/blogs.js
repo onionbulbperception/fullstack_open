d34816e570a988a4e495b4e82bc96a975cc78003
onionbulbperception <ezpr.flip303@passinbox.com>
2025-04-20 13:09:22 +0300

    4.13 Blog List Expansions, step 1

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,21 +1,45 @@
 const blogsRouter = require('express').Router()
-const blog = require('../models/blog')
+const { request, response } = require('express')
 const Blog = require('../models/blog')
 
+// Get all
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
 
+// Get by id
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+// Post
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    const body = request.body
 
-    if (!blog.likes) blog.likes = 0
+    const blog = new Blog({
+      title: body.title,
+      url: body.url,
+      likes: body.likes,
+      user: body.user
+    })
 
-    if (!blog.title || !blog.url) return response.status(400).end
+    const savedBlod = await blog.save()
+    response.status(201).json(savedBlod)
+})
 
-    const result = await blog.save()
-    response.status(201).json(result)
+// Put
+
+
+// Delete
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -128,6 +128,24 @@ describe('blog list test', () => {
 
 })
 
+describe('deletion of a blog', () => {
+  test('succeeds with code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+    const contents = blogsAtEnd.map(r => r.title)
+    assert(!contents.includes(blogToDelete.title))
+  })
+})
+
 describe('when there is initially one user in db', () => {
   beforeEach(async () => {
     await User.deleteMany({})
